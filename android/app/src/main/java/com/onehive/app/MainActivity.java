f56f7064c8ebb6085aff8d80479e608caafff5d3
phpmasterminds <phpmasterminds@gmail.com>
2026-09-24 11:34:59 +0530

    fix error
    fix error

--- android/app/src/main/java/com/onehive/app/MainActivity.java
+++ android/app/src/main/java/com/onehive/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.onehive.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
